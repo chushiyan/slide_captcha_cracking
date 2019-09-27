a2dde85e2ec9a5c1244072487dee93e0f89d1cde
chushiyan <37173602+chushiyan@users.noreply.github.com>
2019-09-28 02:07:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ import urllib.request
 import random
 
 
+#  传入滑块背景图片本地路径和滑块本地路径，返回滑块到缺口的距离
 def findPic(img_bg_path, img_slider_path):
     """
     找出图像中最佳匹配位置
-    传入滑块背景图片和滑块路径，返回滑块到缺口的距离
     :param img_bg_path: 滑块背景图本地路径
     :param img_slider_path: 滑块图片本地路径
     :return: 返回最差匹配、最佳匹配对应的x坐标
@@ -64,6 +64,7 @@ def findPic(img_bg_path, img_slider_path):
     return value[2:][0][0], value[2:][1][0]
 
 
+# 返回两个数组：一个用于加速拖动滑块，一个用于减速拖动滑块
 def generate_tracks(distance):
     # 给距离加上20，这20像素用在滑块滑过缺口后，减速折返回到缺口
     distance += 20
